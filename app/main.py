3e15f38d46e08ab3f3d5f3bd5eeea6ad87464481
DaffaSyauqi <daffasyauqi77@gmail.com>
2025-12-07 18:01:31 +0700

    change allow_origin

--- app/main.py
+++ app/main.py
@@ -9,11 +9,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://backend-python-production-4108.up.railway.app",
-        # "https://umkm-frontend.vercel.app"
-    ], 
+    allow_origins=["*"],
     allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
