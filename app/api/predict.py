b62d61c76bf6d6473cbf13645d5f24e63861c050
DaffaSyauqi <daffasyauqi77@gmail.com>
2025-12-07 18:18:47 +0700

    remove slash

--- app/api/predict.py
+++ app/api/predict.py
@@ -4,7 +4,7 @@ from app.services.predictor_service import predict_production
 
 router = APIRouter(prefix="/predict")
 
-@router.post("/")
+@router.post("")
 async def predict(req: PredictRequest):
     result = predict_production(req.food_name, req.ingredients)
     return {"result": result}
